51394d6addcfcdb49f38bd0ad63a891e32d7ea50
baye94 <fallcheikh893@gmail.com>
2022-09-17 04:11:30 +0000

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=admin123
+export POSTGRES_PASSWORD=admin1234
+export POSTGRES_HOST=admin.cvbljbgiqgtj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=admin
+export AWS_BUCKET=arn:aws:s3:::microservices-projet-udacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
